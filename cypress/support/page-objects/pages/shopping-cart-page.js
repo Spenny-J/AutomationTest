9b71524500a012c4ecf7ce3786b18a59eeba7bdb
Spencer Jensen <kgninja1@gmail.com>
2021-02-12 01:32:14 -0700

    Added shopping cart page to hold references through the shopping cart process

---
+++ cypress/support/page-objects/pages/shopping-cart-page.js
@@ -0,0 +1,10 @@
+/**
+ * Page resource for the shopping cart process of checking out
+ */
+
+class ShoppingCartPage(){
+    proceedToCheckoutBtn(){
+        return cy.get('a[href="http://automationpractice.com/index.php?controller=order&step=1"]');
+    }
+}
+module.export = new ShoppingCartPage();
