0a6e755509d149d5e5a10ea8638c353567c0fb17
Spencer Jensen <kgninja1@gmail.com>
2021-02-11 23:30:11 -0700

    Finished Register Account function

--- cypress/support/page-objects/pages/create-account-page.js
+++ cypress/support/page-objects/pages/create-account-page.js
@@ -5,23 +5,6 @@
  const people = require("../../../../people.json");
 
  class CreateAccountPage{
-        registerAccount(personObj){
-            if(!personObj)
-                personObj = people.default;
-            
-            this.findAccountCreationElement('input#id_gender1').click();
-            this.findAccountCreationElement('input#customer_firstname').type(personObj.firstName);
-            this.findAccountCreationElement('input#customer_lastname').type(personObj.lastName);
-            // Assert the email we sent in the previous test matches the one in the input field
-            this.findAccountCreationElement('input#email').then(()=>{
-                expect(`${this.findAccountCreationElement('input#email')
-                .invoke('val')}`)
-                .should((text) => cy.log(emailText))
-                .to.equal(personObj.email);
-            });
-            this.findAccountCreationElement('input#isPasswd').type(personObj.password);
-        }
-
         /**
          * Function which returns a element within the account creation form
          * @param {string} value 
@@ -30,8 +13,36 @@
             return cy.get('#account-creation_form').find(`${value}`);
         }
 
-        ensurePageLoaded(){
+        accountForm(){
             return cy.get('#account-creation_form');
+        }
+
+        registerAccount(personObj){
+            if(!personObj)
+                personObj = people.default;
+            
+            this.findAccountCreationElement('input#id_gender1').click();
+            this.findAccountCreationElement('input#customer_firstname').type(personObj.firstName);
+            this.findAccountCreationElement('input#customer_lastname').type(personObj.lastName);
+            this.findAccountCreationElement('input#passwd').type(personObj.password);
+            this.findAccountCreationElement('select#days').select(`${personObj.birthDay}`);
+            this.findAccountCreationElement('select#months').select(`${personObj.birthMonth}`);
+            this.findAccountCreationElement('select#years').select(`${personObj.birthYear}`);
+            if(personObj.newsletter === true)
+               this.findAccountCreationElement('input#newsletter').click();
+            if(personObj.specialOffers === true)
+                this.findAccountCreationElement('input#optin').click();
+            this.findAccountCreationElement('input#address1').type(personObj.address);
+            this.findAccountCreationElement('input#address2').type(personObj.addressUnit);
+            this.findAccountCreationElement('input#city').type(personObj.city);
+            this.findAccountCreationElement('select#id_country').select(`${personObj.country}`);
+            this.findAccountCreationElement('select#id_state').select(`${personObj.state}`, {"force": true});
+            this.findAccountCreationElement('input#postcode').type(personObj.zip);
+            this.findAccountCreationElement('textarea#other').type(personObj.additionalInfo);
+            this.findAccountCreationElement('input#phone').type(personObj.homePhone);
+            this.findAccountCreationElement('input#phone_mobile').type(personObj.mobilePhone);
+            this.findAccountCreationElement('input#alias').clear().type(personObj.addressAlias);
+            return this.findAccountCreationElement('button#submitAccount').click();
         }
  } 
 
