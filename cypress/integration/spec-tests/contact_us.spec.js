01ba0c9eef3937dda72f504b718e351654bde4f6
Spencer Jensen <kgninja1@gmail.com>
2021-02-12 01:06:45 -0700

    Added test run and assertion for submitting a contact us form.

--- cypress/integration/spec-tests/contact_us.spec.js
+++ cypress/integration/spec-tests/contact_us.spec.js
@@ -1,14 +1,24 @@
 const DefaultHeaderMenuBar = require("../../support/page-objects/components/default-header-bar");
+const ContactUsPage = require("../../support/page-objects/pages/contact-us-page");
+const Contact = require("../../../contact.json");
 
-describe('Account Creation', ()=>{
-    it('Proceeds through the account creation process', ()=>{
-        //Arrange - Setup the initial state
+describe('Get to Contact Us Page', ()=>{
+    it('Proceeds to the Contact Us Page via the nav bar', ()=>{
         cy.visit('http://automationpractice.com/')
-        //Act - take an action
+        
         DefaultHeaderMenuBar.contactUsBtn().click();
-        //cy.contains('Sign in').click()
-        //Assert - make an assertion
+        
         cy.url()
             .should('include', '/index.php?controller=contact')
-    })
-})
+    });
+
+    it('Submit a contact form', ()=>{
+        Contact.default.email = Contact.default.email.replace("[REPLACE]", Date.now());
+        ContactUsPage.sendContactForm();
+
+        // Assert that the success message is visible
+        // I noticed that this fails on the first submission each time. 
+        // This assertion will catch this issue.
+        cy.get('p.alert.alert-success').should('be.visible');
+    });
+});
